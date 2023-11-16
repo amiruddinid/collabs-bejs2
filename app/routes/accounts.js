686bd88e4efbeab01b8cff6ca38c841019f83b85
afishafiraa <afishafiraa@gmail.com>
2023-11-16 21:38:58 +0700

    create acc rev

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller=require('../controller')
 
 router.post('/api/delete', controller.accounts.delete)
+router.post('/api/create', controller.accounts.create)
 
 module.exports = router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const users = require('./users');
+const accounts = require('./accounts');
+const { accounts } = require('../controller');
 
 router.get("/", (req, res) => {
     res.send("Hello World")
 })
 
 router.use(users);
+router.use(accounts);
 
 module.exports = router;
