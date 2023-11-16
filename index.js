5fe33344f5d56382cd087debe230dd58cfa5cff4
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-11-16 21:40:09 +0700

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const txRoute = require("./router/transactionRoute")
 const port = 3000
 
-app.listen(port, () => 
-    console
-      .log(`Server run at http://localhost:${port}
+app.use("/api/v1/", txRoute);
+app.listen(port, () =>
+  console
+    .log(`Server run at http://localhost:${port}
     `))
 
 // Mengekspor instance aplikasi Express untuk digunakan di file lain
--- router/transactionRoute.js
+++ router/transactionRoute.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const txRoute = express.Router();
+
+txRoute.get("transaction/withdraw", (req, res, next) => {
+    res.status(200).json({ status: "success", message: "withdraw success" })
+})
+
+module.exports = txRoute;
